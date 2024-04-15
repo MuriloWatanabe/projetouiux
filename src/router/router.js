0076111b467a75aefa1718b2a39e3a7ebb49a4f8
Gui <guilherme.gui.tama@gmail.com>
2024-04-15 20:00:11 -0300

    rotas adicionadas ao router

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,19 @@
 import { Route, Routes, BrowserRouter} from "react-router-dom"
 
 //componentes
-import Teste from "../teste"
+import Home from "../pages/Home/home"
+import About from "../pages/About/about"
+import Contact from "../pages/ContactUs/contact"
+import Cart from "../pages/Cart/cart"
 
 const Router = () => {
     return(
     <BrowserRouter>
         <Routes>
-            <Route Component = { Teste } path="/teste" />
+            <Route Component = { Home } exact path="/" />
+            <Route Component = { About } path="/about" />
+            <Route Component = { Contact } path="/contact" />
+            <Route Component = { Cart } path="/cart" />
         </Routes>
     </BrowserRouter>
     )
