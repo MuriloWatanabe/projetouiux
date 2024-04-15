93600850d71e2443a2110bec39020407e302c2a1
Gui <guilherme.gui.tama@gmail.com>
2024-04-15 19:59:29 -0300

    navbar atualizada

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -2,11 +2,19 @@ import "./navbar.css"
 
 export default function Navbar() {
     return(
-        <header>
-            <a href="/">Home</a>
-            <a href="/aboutsobre">Sobre</a>
-            <a href="/ddd">Fale conosco</a>
-            <a href="/cart">Carrinho</a>
+        <header className="navbar">
+            <div className="left-side">
+                <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/about">Sobre</a>
+                <a className="nav-link" href="/contact">Fale conosco</a>
+            </div>
+            <div className="right-side">
+                <a className="nav-link" href="/cart">
+                    <span class="material-icons">
+                        shopping_bag
+                    </span>
+                </a>
+            </div>
         </header>
     )
 }
