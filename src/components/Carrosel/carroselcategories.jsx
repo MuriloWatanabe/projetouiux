c25f37f2105818de83e3c8b82981824e462ebb8c
Gui <guilherme.gui.tama@gmail.com>
2024-04-15 19:58:22 -0300

    carrocel arrumado

---
+++ src/components/Carrosel/carroselcategories.jsx
@@ -0,0 +1,7 @@
+export default function(){
+    return(
+        <div className="imageOP">
+        <img width="500" height="500" src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2024%2F02%2Fone-piece-puma-suede-collection-release-date-4.jpg?cbr=1&q=90"></img>
+      </div>
+    );
+}
