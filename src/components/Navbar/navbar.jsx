1b66ce7b6eaf76779d545c3123ac4aa07ae6426c
Gui <guilherme.gui.tama@gmail.com>
2024-04-15 22:05:53 -0300

    pequenas alterações

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -8,13 +8,20 @@ export default function Navbar() {
                 <a className="nav-link" href="/about">Sobre</a>
                 <a className="nav-link" href="/contact">Fale conosco</a>
             </div>
+            {window.location.pathname == "/" ?
             <div className="right-side">
                 <a className="nav-link" href="/cart">
-                    <span class="material-icons">
+                    <span class="material-icons" id="material-icons">
                         shopping_bag
                     </span>
                 </a>
+                <a className="nav-link" href="/login">
+                    <span class="material-icons" id="material-icons">
+                        person
+                    </span>
+                </a>
             </div>
+            : null}
         </header>
     )
 }
