ae9d0193e12eb1a681b8c25e3aa81dcadfab4c04
guilherme <guilherme.gui.tama@gmail.com>
2024-04-22 19:41:02 -0300

    estilo #materialicons não é mais global, login quase finalizado

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -5,16 +5,19 @@ export default function Login() {
         <div className="container">
 
         <div className="card">
-            <form action="" onClick={(event) => {event.preventDefault()}}>
+            <form id="login-form" action="">
                 <span class="material-icons" id="material-icons">
                     person
                 </span>
             <div className="inputs">
-                <input className="input" type="text" placeholder="email"/>
-                <input className="input" type="text" placeholder="senha"/>
+                <input className="input" type="email" placeholder="email" required/>
+                <input className="input" type="password" placeholder="senha" required/>
             </div>
-            <button className="save-btn">Login</button>
+            <button className="save-btn" type="submit" onSubmit={(e) => {e.preventDefault()}}>Login</button>
             </form>
+            <div>
+                <p className="terms">Ao continuar, afirmo que concordo com a <a className="terms-anchor" href=""> Política de Privacidade</a> e os <a className="terms-anchor" href="/termos">Termos de Uso</a></p>
+            </div>
         </div>
         </div>
     )
