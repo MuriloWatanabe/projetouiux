69a0ca7a1b17396c60d3c36b1fe4a4eecaeb078e
viniihds <viniciushsilva2210@gmail.com>
2024-06-10 20:15:46 -0300

    Correção CSS Footer usando tailwind

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,13 +2,13 @@ import './footer.css';
 
 export default function Footer() {
  return(
-    <footer className="footer">
+    <footer class="bg-black text-white text-center p-[20px] w-full mt-[20px] bottom-0">
         <div>
             <h3>Acadêmicos:</h3>
             <p> Guilherme Tamanini, Murilo Watanabe, Vinicius</p>
         </div>
         <div>
-            <a href="https://github.com/MuriloWatanabe/projetouiux"><h3>Ir para o GitHub do projeto:</h3></a>
+            <a class="text-white" href="https://github.com/MuriloWatanabe/projetouiux"><h3>Ir para o GitHub do projeto:</h3></a>
         </div>
         <div>
             <p>© 2024</p>
