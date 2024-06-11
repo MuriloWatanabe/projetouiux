b07d9ec533b23f371cc697b4d47b8dc7dbb009a6
viniihds <viniciushsilva2210@gmail.com>
2024-06-10 21:39:04 -0300

    Correção CSS Cart Page usando tailwind

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './cart.css';
 import { Link } from 'react-router-dom';
 function CartItem({ id, imgUrl, itemName, itemPrice, quantity, onDelete }) {
     const [itemQuantity, setItemQuantity] = useState(quantity);
@@ -19,25 +18,25 @@ function CartItem({ id, imgUrl, itemName, itemPrice, quantity, onDelete }) {
     };
 
     return (
-        <div className="cart-item">
+        <div class="flex align-center mb-[20px] bg-white p-[10px] rounded-md border-2 border-solid border-slate-100 mx-[30px]">
             <div>
-                <img src={imgUrl} width={250} height={250} alt={itemName} />
+                <img class="mr-[20px]" src={imgUrl} width={250} height={250} alt={itemName} />
             </div>
-            <div className="cart-details">
+            <div class="grow mt-[25px]">
                 <p>{itemName}</p>
                 <p>Preço total: R$ {itemPrice * itemQuantity}</p>
             </div>
-                <div className='qcontrols'>
-                    <button onClick={decreaseQuantity}>-</button>
+                <div>
+                    <button class="bg-slate-800 text-white border-0 rounded-md py-[5px] px-[10px] cursor-pointer m-0 m-[5px] transitions-colors ease-in duration-300" onClick={decreaseQuantity}>-</button>
                 </div>
-            <div className="quantity-controls">
-                <input type="number" value={itemQuantity} readOnly />
+            <div>
+                <input class="w-[50px] border-1 border-solid border-slate-100 rounded-md m-0 m-[5px] pl-[20px] pr-[0px]" type="number" value={itemQuantity} readOnly />
             </div>
-                <div className='qcontrols'>
+                <div>
                     <button onClick={increaseQuantity}>+</button>
                 </div>
             <div className="btn-del-container">
-                <button className="btn-del" onClick={handleDelete}>
+                <button class="bg-red-700 text-color-300 rounded-md py-[5px] px-[10px]" onClick={handleDelete}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px">
                         <path d="M267.33-120q-27.5 0-47.08-19.58-19.58-19.59-19.58-47.09V-740H160v-66.67h192V-840h256v33.33h192V-740h-40.67v553.33q0 27-19.83 46.84Q719.67-120 692.67-120H267.33Zm425.34-620H267.33v553.33h425.34V-740Zm-328 469.33h66.66v-386h-66.66v386Zm164 0h66.66v-386h-66.66v386ZM267.33-740v553.33V-740Z" />
                     </svg>
@@ -59,7 +58,7 @@ export default function Cart() {
 
     return (
         <div>
-            <h1>Carrinho</h1>
+            <h1 class="m-[15px]">Carrinho</h1>
             {cartItems.map(item => (
                 <CartItem
                     key={item.id}
@@ -71,8 +70,8 @@ export default function Cart() {
                     onDelete={handleDeleteItem}
                 />
             ))}
-            <div className="btn-finish-container">
-            <Link  className="btnF" to="/Finish">Finalizar Compra</Link>
+            <div class="text-center mt-[20px]">
+            <Link  class="bg-black text-white border-0 rounded-md py-[10px] px-[20px] cursor-pointer transitions-colors ease-in duration-300" to="/Finish">Finalizar Compra</Link>
             </div>
         </div>
     );
