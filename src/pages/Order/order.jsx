10dd0a1dcfee6cfe7b534fc53be761a36c361bc8
viniihds <viniciushsilva2210@gmail.com>
2024-06-17 20:00:33 -0300

    Modal detalhes do pedido finalizado(ou quase)

--- src/pages/Order/order.jsx
+++ src/pages/Order/order.jsx
@@ -6,15 +6,9 @@ export default function Order() {
 
     const [showModal, setShowModal] = useState(false);
 
-    function closeWhenOpen(){
-        if(showModal){
-            setShowModal(false)
-        }
-    }
-
     return(
         <>
-        <div onClick={closeWhenOpen}>
+        <div>
             <h1>Pedidos</h1>
             <div className="cart-container">
                 <div><img src="https://static.netshoes.com.br/produtos/tenis-casual-masculino-olympikus-drift/04/2I2-2484-004/2I2-2484-004_zoom4.jpg?ts=1624644882" width={250} height={250} alt="" /></div>
@@ -55,49 +49,28 @@ export default function Order() {
                         ></div>
                         <div className="flex items-center min-h-screen px-4 py-8">
                             <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+                            <div className="flex justify-end cursor-pointer">
+                                <h3 onClick={() => setShowModal(false)}>X</h3>  
+                            </div>
                                 <div className="mt-3 sm:flex">
-                                    <div className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="w-6 h-6 text-red-600"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <div className="mt-2 text-center sm:ml-4 sm:text-left">
+                                <div><img src="https://static.netshoes.com.br/produtos/tenis-casual-masculino-olympikus-drift/04/2I2-2484-004/2I2-2484-004_zoom4.jpg?ts=1624644882" width={250} height={250} alt="" /></div>
+
+                                    <div className="mt-2 sm:ml-4 sm:text-left">
                                         <h4 className="text-lg font-medium text-gray-800">
-                                            Delete account ?
+                                            Tênis
                                         </h4>
                                         <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
-                                            Lorem ipsum dolor sit amet,
-                                            consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore
-                                            et dolore magna aliqua.
+                                            Preço: R$444
                                         </p>
-                                        <div className="items-center gap-2 mt-3 sm:flex">
-                                            <button
-                                                className="w-full mt-2 p-2.5 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
-                                                onClick={() =>
-                                                    setShowModal(false)
-                                                }
-                                            >
-                                                Delete
-                                            </button>
-                                            <button
-                                                className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
-                                                onClick={() =>
-                                                    setShowModal(false)
-                                                }
-                                            >
-                                                Cancel
-                                            </button>
-                                        </div>
+                                        <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
+                                            Quantidade: 1
+                                        </p>
+                                        <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
+                                            Tamanho: 42
+                                        </p>
+                                        <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
+                                            Marca: Olympikus
+                                        </p>
                                     </div>
                                 </div>
                             </div>
