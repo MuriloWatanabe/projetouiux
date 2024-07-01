74f0697ce6755a28e670bdf20516da7b278bc736
murilowatanabe <muriloenzowatanabe14@gmail.com>
2024-07-01 19:43:09 -0300

    ajuste cart

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 function CartItem({ id, imgUrl, itemName, itemPrice, quantity, onDelete }) {
     const [itemQuantity, setItemQuantity] = useState(quantity);
 
@@ -18,26 +19,22 @@ function CartItem({ id, imgUrl, itemName, itemPrice, quantity, onDelete }) {
     };
 
     return (
-        <div class="flex align-center mb-[20px] bg-white p-[10px] rounded-md border-2 border-solid border-slate-100 mx-[30px]">
+        <div className="flex items-center mb-4 bg-white p-4 rounded-md border border-gray-200 mx-4">
             <div>
-                <img class="mr-[20px]" src={imgUrl} width={250} height={250} alt={itemName} />
+                <img className="mr-4" src={imgUrl} width={250} height={250} alt={itemName} />
             </div>
-            <div class="grow mt-[25px]">
+            <div className="flex-grow mt-4">
                 <p>{itemName}</p>
                 <p>Preço total: R$ {itemPrice * itemQuantity}</p>
             </div>
-                <div>
-                    <button class="bg-slate-800 text-white border-0 rounded-md py-[5px] px-[10px] cursor-pointer m-0 m-[5px] transitions-colors ease-in duration-300" onClick={decreaseQuantity}>-</button>
-                </div>
-            <div>
-                <input class="w-[50px] border-1 border-solid border-slate-100 rounded-md m-0 m-[5px] pl-[20px] pr-[0px]" type="number" value={itemQuantity} readOnly />
+            <div className="flex items-center">
+                <button className="bg-black text-white border-0 rounded-md py-1 px-2 cursor-pointer transition-colors duration-300" onClick={decreaseQuantity}>-</button>
+                <input className="w-16 border border-gray-20 rounded-md m-1 pl-2 text-center" type="number" value={itemQuantity} readOnly />
+                <button className="bg-black text-white border-0 rounded-md py-1 px-2 cursor-pointer transition-colors duration-300" onClick={increaseQuantity}>+</button>
             </div>
-                <div>
-                    <button onClick={increaseQuantity}>+</button>
-                </div>
-            <div className="btn-del-container">
-                <button class="bg-red-700 text-color-300 rounded-md py-[5px] px-[10px]" onClick={handleDelete}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px">
+            <div className="ml-4">
+                <button className="bg-red-700 text-white rounded-md py-1 px-2" onClick={handleDelete}>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20">
                         <path d="M267.33-120q-27.5 0-47.08-19.58-19.58-19.59-19.58-47.09V-740H160v-66.67h192V-840h256v33.33h192V-740h-40.67v553.33q0 27-19.83 46.84Q719.67-120 692.67-120H267.33Zm425.34-620H267.33v553.33h425.34V-740Zm-328 469.33h66.66v-386h-66.66v386Zm164 0h66.66v-386h-66.66v386ZM267.33-740v553.33V-740Z" />
                     </svg>
                 </button>
@@ -57,8 +54,8 @@ export default function Cart() {
     };
 
     return (
-        <div>
-            <h1 class="m-[15px]">Carrinho</h1>
+        <div className="p-4">
+            <h1 className="my-4 text-2xl font-bold">Carrinho</h1>
             {cartItems.map(item => (
                 <CartItem
                     key={item.id}
@@ -70,8 +67,8 @@ export default function Cart() {
                     onDelete={handleDeleteItem}
                 />
             ))}
-            <div class="text-center mt-[20px]">
-            <Link  class="bg-black text-white border-0 rounded-md py-[10px] px-[20px] cursor-pointer transitions-colors ease-in duration-300" to="/Finish">Finalizar Compra</Link>
+            <div className="text-center mt-4">
+                <Link to="/Finish" className="bg-black text-white rounded-md py-2 px-4 cursor-pointer transition-colors duration-300">Finalizar Compra</Link>
             </div>
         </div>
     );
